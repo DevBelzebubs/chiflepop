602dc77b74cfa3d6ff8714792b4d00d5429f0ed4
Juan Diego <juandiego.gr05@gmail.com>
2025-10-13 20:31:20 -0500

    GR-011 fixed some things, must to improve responsive to bottom bar

--- src/ui/Footer/BottomBarUI.tsx
+++ src/ui/Footer/BottomBarUI.tsx
@@ -4,25 +4,26 @@ const BottomBarUI = () => {
     const socialMedia = ['/img/Facebook.svg','/img/Instagram.svg','/img/WhatsApp.svg']
   return (
     <div className='relative w-full bg-no-repeat bg-cover z-50 flex justify-center' style={{backgroundImage:"url('/img/BottomBarBackground.svg')"}}>
-        <div className='flex items-center pl-10 gap-[30vw] mt-[20vh]'>
+        <div className='flex items-center pl-10 gap-[30vw] mt-[20vh] mb-14'>
             <img src='/img/LogoFotter.svg' width={"320px"}/>
-            <div className='flex items-center gap-5 bg-white rounded-2xl p-5' style={{boxShadow: "0 4px 10px 0 rgb(0,0,0,0.25)"}}>
+            <div className='flex items-center gap-5 bg-white rounded-2xl p-5 pt-7 pb-7' style={{boxShadow: "0 4px 10px 0 rgb(0,0,0,0.25)"}}>
                 <div className='flex flex-col gap-5'>
-                    <h2>Contáctanos</h2>
+                    <h2 style={{fontSize: "24px"}}>Contáctanos</h2>
                     <div>
                         <p>Telf:923 049 677</p>
                         <p>Email: contactos@chiflepop.com</p>
                         <p>Horario: Atención las 24 horas</p>
                     </div>
                 </div>
-                <div>
-                    <h2>Conecta con nosotros</h2>
-                    <div className='flex gap-5'>
+                <div className='flex flex-col items-center'>
+                    <h2 style={{fontSize: "24px"}}>Conecta con nosotros</h2>
+                    <div className='flex gap-6'>
                         {socialMedia.map((opcion)=>(
-                            <img src={opcion} width={'48px'}/>
+                            <img src={opcion} width={'42px'}/>
                         ))}
                     </div>
-                    <div>
+                    <div className='flex items-center gap-2 mt-1 ml-2'>
+                        <img src='/img/libro-abierto.svg' width={"34px"}/>
                         <p>Libro de reclamaciones</p>
                     </div>
                 </div>
